fe480390ec28e355414e8726f3ccf664fe6ebce2
hjbar <hjbar@protonmail.com>
2025-02-24 00:00:40 +0100

    Fix reduction for parallelism

--- src/algo/reduction.ml
+++ src/algo/reduction.ml
@@ -19,15 +19,10 @@ let rule_3 g k s = (g, k, s)
 
 (* I believe rules 4 to 7 cannot trigger each other or themselves *)
 
-(* This is a messy workaround. *)
-(* It also only works because of some internal knowledge about Graph: *)
-(* We know that get_neighbors_list returns the neighbors in sorted ascending order. *)
-let rule_5_6_helper : (int * int, int) Hashtbl.t = Hashtbl.create 100
-
 (* There's a slight hack here that loses in performance, but that has no easy solution.
  * It may perform some redundant checks performed by rule_5_6_row_part2.
  * The alternative is to have g' be a directed graph, or some other comnpromise. *)
-let rule_5_6_row_part1 g' g i =
+let rule_5_6_row_part1 rule_5_6_helper g' g i =
   let open Graph.GraphNotation in
   match Graph.get_neighbors_list g i with
   | [ u1; u2 ] when Graph.is_black g u1 && Graph.is_black g u2 ->
@@ -37,27 +32,27 @@ let rule_5_6_row_part1 g' g i =
       match (degu1', degu2') with
       | 0, _ ->
         (* u1 is of degree < 4 *)
-        Hashtbl.add rule_5_6_helper (u1, u2) i;
+        Hashtbl.replace rule_5_6_helper (u1, u2) i;
         let nu1 = Graph.get_neighbors_list g u1 in
         if List.exists (fun x -> g @? (x, u2)) nu1 then g // i else g
       | _, 0 ->
         (* u2 is of degree < 4 *)
-        Hashtbl.add rule_5_6_helper (u1, u2) i;
+        Hashtbl.replace rule_5_6_helper (u1, u2) i;
         let nu2 = Graph.get_neighbors_list g u2 in
         if List.exists (fun x -> g @? (x, u1)) nu2 then g // i else g
       | d1, _ when d1 < 8 ->
-        Hashtbl.add rule_5_6_helper (u1, u2) i;
+        Hashtbl.replace rule_5_6_helper (u1, u2) i;
         let nu1 = Graph.get_neighbors_list g' u1 in
         if List.exists (fun x -> g @? (x, u2)) nu1 then g // i else g
       | _, d2 when d2 < 8 ->
-        Hashtbl.add rule_5_6_helper (u1, u2) i;
+        Hashtbl.replace rule_5_6_helper (u1, u2) i;
         let nu2 = Graph.get_neighbors_list g' u2 in
         if List.exists (fun x -> g @? (x, u1)) nu2 then g // i else g
       | _ -> g
     end
   | _ -> failwith "mismatch rule 5/6 pt1"
 
-let rule_5_6_row_part2 g i =
+let rule_5_6_row_part2 rule_5_6_helper g i =
   let open Graph.GraphNotation in
   let rem (u1, u2) g =
     begin
@@ -84,11 +79,12 @@ let rule_5_6_row_part2 g i =
   end
   | _ -> g
 
-let rule_5_6' g =
+let rule_5_6' rule_5_6_helper g =
   let g' = Graph.g_over_4 g in
   g
-  |> Graph.map_like @@ Graph.on_deg 2 @@ Graph.on_white @@ rule_5_6_row_part1 g'
-  |> Graph.map_like rule_5_6_row_part2
+  |> Graph.map_like @@ Graph.on_deg 2 @@ Graph.on_white
+     @@ rule_5_6_row_part1 rule_5_6_helper g'
+  |> Graph.map_like (rule_5_6_row_part2 rule_5_6_helper)
 
 (* Technically, checking for Graph.is_black is redundant,
  * since i is white and rule 1 has been applied. *)
@@ -126,9 +122,12 @@ let reduce_cautious (g : Graph.t) (wnew : int list) (k : int) (s : int list) :
 
   let g, k, s = rule_3 g k s in
 
-  let g = g |> rule_4_7 |> rule_5_6' in
+  (* This is a messy workaround. *)
+  (* It also only works because of some internal knowledge about Graph: *)
+  (* We know that get_neighbors_list returns the neighbors in sorted ascending order. *)
+  let rule_5_6_helper = Hashtbl.create 16 in
+  let g = g |> rule_4_7 |> rule_5_6' rule_5_6_helper in
 
-  Hashtbl.clear rule_5_6_helper;
   rule_3 g k s
 
 (* ===== Paper Implementation ===== *)
@@ -140,18 +139,14 @@ let rec_graph g v' : Graph.t =
 
 (* Parallel stuff *)
 
-(*
 exception Stop
-
-let stop = Atomic.make false
-*)
 
 (* Black vertices are non-dominated *)
 (* White vertices are dominated *)
-let rec dominating_k_aux (g : Graph.t) (wnew : int list) (k : int) (s : int list)
-  : int list option =
+let rec dominating_k_aux (g : Graph.t) (stop : bool Atomic.t) (wnew : int list)
+  (k : int) (s : int list) : int list option =
   (* Parallel stop ? *)
-  (* if Atomic.get stop then raise Stop; *)
+  if Atomic.get stop then raise Stop;
 
   (* Preprocessing *)
   let g, k, s = reduce_cautious g wnew k s in
@@ -173,12 +168,11 @@ let rec dominating_k_aux (g : Graph.t) (wnew : int list) (k : int) (s : int list
     in
 
     let rec_f v' =
-      dominating_k_aux (rec_graph g v')
+      dominating_k_aux (rec_graph g v') stop
         (Graph.get_neighbors_list g v')
         (k - 1) (v' :: s)
     in
 
-    (* let s' = List.find_map rec_f b' in *)
     let s' =
       List.fold_left
         begin
@@ -190,7 +184,6 @@ let rec dominating_k_aux (g : Graph.t) (wnew : int list) (k : int) (s : int list
         end
         None b'
     in
-    (* if s' = None then List.find_map rec_f w' else s' *)
     if s' = None then
       List.fold_left
         begin
@@ -203,36 +196,12 @@ let rec dominating_k_aux (g : Graph.t) (wnew : int list) (k : int) (s : int list
         None w'
     else s'
 
-let dominating_k (g : Graph.t) (k : int) : int list option =
-  (* try dominating_k_aux g [] k [] with Stop -> None *)
-  dominating_k_aux g [] k []
-
-(* Note : huge inefficiency here *)
-let rec dominating_binsearch (g : Graph.t) (min : int) (max : int)
-  (smax : int list) : int list =
-  match max - min with
-  | 0 -> failwith "Binary search failed to find a dominating set"
-  | 1 -> smax
-  | _ ->
-    let k = (max + min) / 2 in
-    begin
-      match dominating_k g k with
-      | Some s -> dominating_binsearch g min k s
-      | None -> dominating_binsearch g k max smax
-    end
+let dominating_k (g : Graph.t) (k : int) (stop : bool Atomic.t) :
+  int list option =
+  try dominating_k_aux g stop [] k [] with Stop -> None
 
 let dominating_paper (g : Graph.t) (min : int) (max : int) : int list =
-  match dominating_k g min with
-  | Some s -> s (* Minimum dominating set size is viable *)
-  | None -> begin
-    match dominating_k g max with
-    | None -> failwith "Maximum dominating set size is not viable"
-    | Some s -> dominating_binsearch g min max s
-  end
-
-(*
-let dominating_paper (g : Graph.t) (min : int) (max : int) : int list =
-  Atomic.set stop false;
+  let stop = Atomic.make false in
 
   let nb_domains = Domain.recommended_domain_count () in
   let c = float (max - min) /. float nb_domains |> Float.ceil |> int_of_float in
@@ -243,19 +212,14 @@ let dominating_paper (g : Graph.t) (min : int) (max : int) : int list =
     |> List.sort_uniq compare
   in
 
-  List.iter (fun i -> Format.printf "%d %!" i) args;
-  Format.printf "@\n%!";
-
   let doms =
     List.map
       begin
         fun n ->
           Domain.spawn @@ fun () ->
-          match dominating_k (Graph.copy g) n with
+          match dominating_k (Graph.copy g) n stop with
           | Some _ as res when not @@ Atomic.get stop ->
             Atomic.set stop true;
-            Format.printf "Res of f(%d) = %d@\n%!" n
-              (List.length @@ Option.get res);
             res
           | _ -> None
       end
@@ -276,7 +240,6 @@ let dominating_paper (g : Graph.t) (min : int) (max : int) : int list =
   in
 
   match res with None -> failwith "No solution" | Some res -> res
-*)
 
 (* == Main Function == *)
 
